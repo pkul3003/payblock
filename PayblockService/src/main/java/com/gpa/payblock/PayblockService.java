baf9a6a7e7456c4394c82e2cda808f3f44292664
Parag <pkul3003@gmail.com>
2017-07-25 01:08:47 +0530

    Add files via upload

---
+++ PayblockService/src/main/java/com/gpa/payblock/PayblockService.java
@@ -0,0 +1,22 @@
+package com.gpa.payblock;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/hash")
+public class PayblockService {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMsg() {
+
+		String output = "Returning hash from ethereum ";
+
+		return output;
+
+	}
+}
